f6f35fe2773c7594f174307f3c99ae7f67be6dc6
SungJin-Jin <deskhi@nethru.co.kr>
2018-08-10 00:12:42 +0900

    Add global url pattern based

--- src/main/kotlin/com/sc/security/SecurityApplication.kt
+++ src/main/kotlin/com/sc/security/SecurityApplication.kt
@@ -2,9 +2,28 @@ package com.sc.security
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+@Configuration
+@EnableCaching
 @SpringBootApplication
-class SecurityApplication
+class SecurityApplication : WebMvcConfigurer {
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(false)
+                .maxAge(3600)
+
+        super.addCorsMappings(registry)
+    }
+
+}
 
 fun main(args: Array<String>) {
     runApplication<SecurityApplication>(*args)
